b8977d38e9c5493fecacebfe1fa40ea8f75d0b9f
xschur <ring0sec@gmail.com>
2022-11-23 10:59:43 +0800

    update checkin time

--- clockIn/req_model.py
+++ clockIn/req_model.py
@@ -79,7 +79,6 @@ class ClockIn:
         jsonobj = self.__get_old_info()
         if jsonobj == "":
             return ""
-        logging.info(jsonobj)
         res = self.__session.post(url=self.__upload_url, headers=self.__headers, data=jsonobj)
         return res.text
 
